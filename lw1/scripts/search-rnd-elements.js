5a2dee063cb85a758c09f3ef22a592965bc2fb4f
AlexeevPavel <31746432+AlexeevPavel@users.noreply.github.com>
2017-11-25 13:22:50 +0300

    Update and rename search-rnd-el.js to search-rnd-elements.js

---
+++ lw1/scripts/search-rnd-elements.js
@@ -0,0 +1,24 @@
+const arrMax = 10;
+var RndArray = [];
+var element;
+var i;
+ 
+for(i = 1; i <= arrMax; i++) {
+  RndArray[i] = Math.floor(Math.random() * (15 - 1)) + 1;	
+}
+
+var element = prompt("Enter the number: ");
+
+if ((element == "") || (isNaN(element) || (element == null))) {
+  alert("Error!");
+  windows.stop;
+}
+
+if (RndArray.indexOf(element) !== -1) {
+  Document.write('Element '+ element + ' is found!');
+  Document.write(RndArray.join());
+} 
+else {
+  Document.write('Element '+ element + ' not found!');
+  Document.write(RndArray.join(" "));
+}
