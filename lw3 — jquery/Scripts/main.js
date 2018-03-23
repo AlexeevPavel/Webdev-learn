f44ab87f319151bac2849556555d9e26000691a1
Pavel <alexeevpavel2014@gmail.com>
2018-03-23 21:18:19 +0300

    add conditions

--- lw3 — jquery/Scripts/main.js
+++ lw3 — jquery/Scripts/main.js
@@ -6,5 +6,23 @@ $(document).ready(function () {
     var passConfirm = $(".password-reg-repeat").val();
     var checkbox = $(".check-reg").prop("checked");
     var result = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email));
+    
+    if ((email.length < 5) || (!result)) {
+      alert("E-Mail введен неправильно (example@example.example)");
+    } else {
+      if (pass.length < 6) {
+        alert("Пароли должны быть неменее 6-ти символов и совпадать.");
+      } else {
+        if (pass != passConfirm) {
+          alert("Пароли должны быть неменее 6-ти символов и совпадать.");
+        } else {
+          if (!checkbox) {
+            alert("Примите условия соглашения.");
+          } else {
+            alert("Регистрация завершенна");
+          }
+        }
+      }
+    }
   });
 });
