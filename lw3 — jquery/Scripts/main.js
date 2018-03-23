ebca42196b91b4c4002c124356c51c55d72904ae
Pavel <alexeevpavel2014@gmail.com>
2018-03-23 21:17:34 +0300

    Add variables

--- lw3 — jquery/Scripts/main.js
+++ lw3 — jquery/Scripts/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-  alert("Congretulation!");
+  $('.registration').submit(function (event) {
+    event.preventDefault();
+    var email = $(".email-reg").val();
+    var pass = $(".password-reg").val();
+    var passConfirm = $(".password-reg-repeat").val();
+    var checkbox = $(".check-reg").prop("checked");
+    var result = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email));
+  });
 });
